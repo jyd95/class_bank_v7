c90149ba5909cc1fea0b8efda299f593ec9d8348
JunYeong Doh <secondclover714@gmail.com>
2024-08-06 14:23:16 +0900

    mapper 생성및 추가 설정

---
+++ src/main/java/com/tenco/bank/repository/interfaces/AccountRepository.java
@@ -0,0 +1,24 @@
+package com.tenco.bank.repository.interfaces;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.tenco.bank.repository.model.Account;
+
+@Mapper
+public interface AccountRepository {
+	public int insert(Account account);
+	public int updateById(Account account);
+	public int deleteById(Integer id);
+	
+	// 계좌 조회 기능 고민해보자.
+	// 한사람의 유저는 여러개의 계좌 번호를 가질 수 있다.
+	public List<Account> findByUserId(@Param("userId") Integer principalId);
+	
+	// account id 값으로 계좌 정보 조회
+	public Account findByNumber(@Param("number") String id);
+	
+	
+}
